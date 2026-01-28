5ec8eb61aac64874b3a57f54960c01a7c4603654
Lowking0330 <lowking0330@gmail.com>
2026-01-28 11:35:26 +0800

    Update podcast title in app.py

--- app.py
+++ app.py
@@ -10,5 +10,5 @@ with streamlit_analytics.track(): # <--- 2. 包裝所有邏輯
     st.set_page_config(page_title="原語 Podcast", layout="wide", initial_sidebar_state="expanded") 
     
     # ... (您的所有 UI 程式碼，從這裡開始都需要縮排)
-    st.title("🎙️ 族語Podcast內容產製程式")
+    st.title("🎙️ 族語廣播及Podcast內容產製程式")
     # ...
